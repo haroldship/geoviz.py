2f8aba6d7d2dd87849a5af929be9c3e0b46e707c
Harold Ship <harold.ship@gmail.com>
2016-02-21 10:16:01 +0200

    initial commit to see if it works

---
+++ geoviz.py
@@ -0,0 +1,17 @@
+#
+#
+#
+print 'loading geoviz.py'
+
+class Map(object):
+    url: None
+    endpoint: None
+    data: None
+    style: None
+
+def publish(data, style, endpoint)
+    map = Map()
+    map.url = endpoint + '/stuff/goes/here'
+
+print 'geoviz.py loaded'
+
