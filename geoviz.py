6023249470448c573fc58fda1e7089afc67f6617
Harold Ship <harold.ship@gmail.com>
2016-02-21 13:42:19 +0200

    update it

--- geoviz.py
+++ geoviz.py
@@ -4,14 +4,25 @@
 print 'loading geoviz.py'
 
 class Map(object):
-    url = None
-    endpoint = None
-    data = None
-    style = None
+    def __init__(self, data, style, endpoint=None, url=None):
+        self.data = data
+        self.style = style
+        self.endpoint = endpoint
+        self.url = url
+        if self.url and not self.endpoint:
+            self.endpoint = self.url
 
-def publish(data, style, endpoint):
-    map = Map()
-    map.url = endpoint + '/stuff/goes/here'
+    def publish(self):
+        self.url = self.endpoint + '/path/to/something'
+        return self.url
+
+    def update(self):
+        if not self.url:
+            raise RuntimeError('cannot update without url') 
+        pass
+
+    def show(self):
+        return '<iframe src="http://www.yahoo.com">'
 
 print 'geoviz.py loaded'
 
