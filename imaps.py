311b4d31f0e683f6e19256a19b9c8a3b2a180ba6
Harold Ship <harold.ship@gmail.com>
2016-02-22 16:20:51 +0200

    imaps.py

---
+++ imaps.py
@@ -0,0 +1,25 @@
+#
+#
+#
+
+class Map(object):
+    def __init__(self, data, endpoint, apikey=None, apikey_password=None, mapid=None):
+        self.data = data
+        self.endpoint = endpoint
+        if endpoint[-1] != '/':
+            endpoint += '/'
+        self.apikey = apikey
+        self.apikey_password = apikey_password
+        self.style = self.endpoint + self.mapid
+
+    def publish(self):
+        return self.url
+
+    def update(self, style):
+        if not self.url:
+            raise ValueError('cannot update without url') 
+        self.style = style
+
+
+
+
